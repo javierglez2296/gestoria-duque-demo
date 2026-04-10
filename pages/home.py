70f1bf6dddccb0a9ec2d8ed4d9483432be65764b
javierglez2296 <81320903+javierglez2296@users.noreply.github.com>
2026-04-10 10:47:35 +0200

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,17 +1,17 @@
 import dash
 from dash import html
+
 from components.home.hero import build_hero
 
 dash.register_page(
     __name__,
     path="/",
-    title="Inicio",
+    title="Gestoría en Ávila",
     name="Inicio",
 )
 
 layout = html.Div(
     [
         build_hero(),
-    ],
-    className="page-shell",
+    ]
 )
